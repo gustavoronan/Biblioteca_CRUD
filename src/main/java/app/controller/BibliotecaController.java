d4d25e4cad25a2ffb8218caf37c8fd9fd0694682
gustavo <gustavoronn@gmail.com>
2024-02-22 23:28:56 -0300

    controller update aplicada

--- src/main/java/app/controller/BibliotecaController.java
+++ src/main/java/app/controller/BibliotecaController.java
@@ -3,7 +3,9 @@ package app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,10 +37,30 @@ public class BibliotecaController {
 
 			return new ResponseEntity<String>("Deu esse erro aqui: "+e.getMessage(), HttpStatus.BAD_REQUEST);
 
-		}
+		}		
+		
+	}
+	
+	@PutMapping("update/{idBiblioteca}")
+	public ResponseEntity<String> save(@RequestBody BibliotecaEntity biblioteca, @PathVariable int idBiblioteca){
 
+
+
+		try {
+
+			String mensagem = this.bibliotecaService.update(idBiblioteca, biblioteca);
+			return new ResponseEntity<String>(mensagem,HttpStatus.OK);
+
+
+
+
+		} catch (Exception e) {
+
+			return new ResponseEntity<String>("Deu esse erro aqui: "+e.getMessage(), HttpStatus.BAD_REQUEST);
+
+		}		
+		
 	}
 
 }
-
 
--- src/main/java/app/service/BibliotecaService.java
+++ src/main/java/app/service/BibliotecaService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import app.entity.BibliotecaEntity;
+import app.entity.Carro;
 
 
 @Service
@@ -17,5 +18,44 @@ public class BibliotecaService {
 		lista.add(biblioteca);
 		return biblioteca.getNome() + " salva com sucesso";
 	}
+	
+	
+
+	public List<BibliotecaEntity> listAll(){
+
+		BibliotecaEntity biblioteca = new BibliotecaEntity(1, "AAA", "BBB");
+		BibliotecaEntity biblioteca2 = new BibliotecaEntity(2, "ABC", "BBB");
+		BibliotecaEntity biblioteca3 = new BibliotecaEntity(3, "DDD", "BBB");
+
+		lista.add(biblioteca);
+		lista.add(biblioteca2);
+		lista.add(biblioteca3);
+
+		return lista;
+
+	}
+
+	
+	
+	public String update (long idBiblioteca, BibliotecaEntity biblioteca) {
+		
+		lista = this.listAll();
+		
+		if (lista!=null) {
+			for (int i=0; i<lista.size(); i++) {
+				if(lista.get(i).getId() == idBiblioteca) {
+					lista.set(i, biblioteca);
+					return biblioteca.getNome() + " alterado com sucesso";
+
+				}
+			}
+			
+		}else {
+			
+			return "biblioteca nao encontrada.";
+		}
+			
+	}
+	
 	
 }
